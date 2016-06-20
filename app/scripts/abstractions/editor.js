b9c5f5a3a627379728d63f3ffab29c3d9ce684df
Nick Perez <nperez0111@gmail.com>
2016-06-20 00:21:38 -0400

    Inserting html works

--- app/scripts/abstractions/editor.js
+++ app/scripts/abstractions/editor.js
@@ -1,9 +1,36 @@
 var Medium = require( 'Medium.js' );
-module.exports = function ( el ) {
-    return new Medium( {
-        element: el,
-        mode: Medium.partialMode,
-        placeholder: 'it Works',
-        autofocus: false
-    } )
+module.exports = function ( options ) {
+    var editor = new Medium( Object.assign( {
+            mode: Medium.partialMode,
+            placeholder: 'it Works',
+            autofocus: false
+        }, options ) ),
+        manip = {
+            insHTML: function ( html, callback ) {
+                editor.focus();
+                editor.insertHtml( html, callback );
+                console.log( 'test' )
+                return manip;
+            },
+            destroy: function () {
+                editor.destroy();
+                return manip;
+            },
+            undo: function () {
+                editor.undo();
+                return manip;
+            },
+            redo: function () {
+                editor.redo();
+                return manip;
+            },
+            getCurVal: function () {
+                return editor.value( undefined );
+            },
+            setCurVal: function ( val ) {
+                editor.value( val );
+                return manip;
+            }
+        };
+    return manip;
 }
