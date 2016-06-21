97c9c35c63a39ef3f675b63ba5aa7f9223a43fb7
Maria Perez <mariaperez@Marias-iMac.local>
2016-06-21 11:44:23 -0400

    Actually seperated the two views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,13 +2,21 @@
 
 var Ractive = require( 'ractive/ractive.min' );
 Ractive.DEBUG = /unminified/.test( function () { /*unminified*/ } );
-var GraphEditor = require( './views/GraphEditor' );
+var GraphEditor = require( './views/GraphEditor' ),
+	Graph=require('./views/Graph');
+	
 window.Router = require( './utils/Router' );
+
 Router.route( {
     "Graph/editor": function () {
         return new GraphEditor( {
             el: '.hook'
         } );
+    },
+    "Graph/view":function(){
+    	return new Graph( {
+    		el: '.hook'
+    	} );
     }
 } );
 Router.base( "Graph/editor" );
